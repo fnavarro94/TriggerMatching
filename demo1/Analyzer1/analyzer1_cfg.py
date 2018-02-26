8b247f9f8833af12275ad09c249b5d26d290eef0
Felipe Navarro <felinavarro94@gmail.com>
2018-02-26 01:49:21 -0300

    montecarlo file added

--- demo1/Analyzer1/analyzer1_cfg.py
+++ demo1/Analyzer1/analyzer1_cfg.py
@@ -9,7 +9,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Photon/AOD/12Oct2013-v1/10000/AC980FCC-943D-E311-A1D6-003048F11824.root'
+         'root://eospublic.cern.ch//eos/opendata/cms/MonteCarlo2011/Summer11LegDR/ZZTo2e2mu_7TeV_mll8_mZZ95-160-powheg15-pythia6/AODSIM/PU_S13_START53_LV6-v1/020000/30FDADFE-4C9D-E411-977F-00266CFFBF30.root'
     )
 )
 
